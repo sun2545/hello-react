40716e2cc58e64776a991b4c4f3fc3f13d792869
sun2545 <kwonsunhye0206@gmail.com>
2018-08-05 17:53:52 -0400

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
 import React, { Component } from 'react';
-import Counter from './Counter';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+  id =2
+  state = {
+    information: [
+    {
+      id:0,
+      name:'hone gil',
+      phone:'905-422-2222'
+    },
+    {
+      id:1,
+      name:'kimchi',
+      phone:'465-311-1111'
+    }
+  ],
+  keyword: ''
+}
+handleChange =(e) => {
+  this.setState({
+   keyword: e.target.value,
+  });
+}
+handleCreate = (data) =>{
+    const{ information } = this.state;
+    this.setState({
+      information:information.concat({id:this.id++,...data})
+    })
+}
+handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+handleUpdate =(id,data) =>{
+    const {information} = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+        ?{...info,...data} 
+        : info
+      )
+    })
+  }
   render() {
-     
-    return (       
-           <Counter/>
-         
+
+    const { information, keyword} = this.state;
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
+    return (
+      <div>
+        <PhoneForm 
+        onCreate ={this.handleCreate}
+        />
+        <p>
+          <input
+          placeholder = "input keyword"
+          onChange ={this.handleChange}
+          value={keyword}
+          />
+          </p>
+          <hr />
+       <PhoneInfoList 
+       data={filteredList}
+       onRemove ={this.handleRemove}
+       onUpdate ={this.handleUpdate}
+       />
+      </div>
     );
   }
 }
